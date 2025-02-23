03b46f0f4e09fab03ecd75b83688510fa8b2c07f
Alfred Morgan <alfred@54.org>
2025-02-22 19:32:08 -0800

    convert .png files to .rgb

---
+++ tools/convertpng.nim
@@ -0,0 +1,21 @@
+# Copyright (c) 2025 by Alfred Morgan <alfred@54.org>
+# License https://opensource.org/license/isc-license-txt
+import std/[cmdline, os, strutils, strformat]
+
+# to install nimPNG use:
+# nimble install nimPNG
+import nimPNG
+
+const EX_USAGE = 64
+
+let args = commandLineParams()
+if args.len == 0:
+    quit(&"usage:\n\t{getAppFilename()} <filename.png> [...]", EX_USAGE)
+
+for fname in args:
+    let png = loadPNG24 fname
+
+    echo &"{fname}: {png.width = }, {png.height = }"
+    let f = open(fname.changeFileExt("rgb"), fmWrite)
+    f.write png.data
+    close f
