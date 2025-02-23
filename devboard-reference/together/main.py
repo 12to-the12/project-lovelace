6208a5ce84167ecceddd73c0e7c32318149fea1a
Alfred Morgan <alfred@54.org>
2025-02-22 19:35:09 -0800

    sprite demo bouncing a raw rgb image around the screen

---
+++ devboard-reference/together/main.py
@@ -0,0 +1,2 @@
+import together
+together.main()
---
+++ devboard-reference/together/together.py
@@ -0,0 +1,38 @@
+from math import sin
+from lcd import *
+
+
+def main():
+    imgWidth = 49
+    imgHeight = 63
+    x = 0
+    xv = 1
+    y = 0
+    yv = 1
+    lcd_init()
+    lcd_clear()
+    while True:
+        lcd_fill(x - 1, y - 1, imgWidth + 2, 2) # TODO: not sure why I had to do height of 2 (should be 1) in order to avoid artifacts
+        lcd_fill(x - 1, y + imgHeight, imgWidth + 2, 2) # TODO: not sure why I had to do height of 2 (should be 1) in order to avoid artifacts
+        lcd_fill(x - 1, y + 1, 1, imgHeight - 1)
+        lcd_fill(x + imgWidth, y + 1, 1, imgHeight - 1)
+        lcd_blit_file("together.rgb", x, y, imgWidth, imgHeight)
+        x += xv
+        if x < 0:
+            x = 0
+            xv = -xv
+        elif x + imgWidth >= width:
+            x = width - imgWidth - 1
+            xv = -xv
+            y += yv
+        y += yv
+        if y < 0:
+            y = 0
+            yv = -yv
+        elif y + imgHeight >= height:
+            y = height - imgHeight - 1
+            yv = -yv
+            y += yv
+
+if __name__ == "__main__":
+    main()
